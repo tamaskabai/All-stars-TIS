6198416364f7e767207e92fa198540435c8a91d5
zrubec6 <zrubec6@h6-a-24.cr.fmph.uniba.sk>
2018-09-28 11:57:21 +0200

    preklad hanoi.c

--- hanoi.c
+++ hanoi.c
@@ -3,11 +3,11 @@
 void hanoi(int n, int a, int b)
 {
   if (n == 1) 
-    printf("move stone 1 from %d to %d\n", a, b);
+    printf("daj kamen 1 s %d na %d\n", a, b);
   else
   {
     hanoi(n - 1, a, 6 - b);
-    printf("move stone %d from %d to %d\n", n, a, b);
+    printf("daj kamen %d z %d na %d\n", n, a, b);
     hanoi(n-1, 6 - a - b, b);
   }
 }
